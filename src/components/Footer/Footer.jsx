9adffadec79c0215d7b156cbb50df882a9cf9708
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-08-02 11:08:17 +0300

    Add Footer component with styling for Loan App

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer className="c-footer">
+            <p className="c-footer__desc">
+                Copyright © 2025 Loan App
+            </p>
+        </footer>
+    );
+}
