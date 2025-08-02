dd45d7b2d7aebb2a72887589121e8e9d1c471ab4
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-08-02 12:18:13 +0300

    Add Modal & Form components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Form from "./components/Form/Form";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
     <>
+      <Modal />
+
       <Header />
+
+      <Form />
 
       <Footer />
     </>
