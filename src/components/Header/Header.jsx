f759294e3e3daae5ec181ff789fa97ba451ab061
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-08-02 11:08:09 +0300

    Add Header component with styling for Loan App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="c-header">
+            <h1 className="c-header__title">Loan App</h1>
+        </header>
+    );
+}
